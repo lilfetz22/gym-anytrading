65c4c4c84318754f275749518a518009d14ab0ce
lilfetz22 <billfetzner@yahoo.com>
2024-03-31 05:45:53 -0400

    added my renko data as the dataset

--- gym_anytrading/datasets/__init__.py
+++ gym_anytrading/datasets/__init__.py
@@ -2,7 +2,7 @@ from .utils import load_dataset as _load_dataset
 
 
 # Load FOREX datasets
-FOREX_EURUSD_1H_ASK = _load_dataset('FOREX_EURUSD_1H_ASK', 'Time')
+FOREX_EURUSD_RENKO = _load_dataset('renko_full_data_81', 'datetime')
 
 # Load Stocks datasets
 STOCKS_GOOGL = _load_dataset('STOCKS_GOOGL', 'Date')
